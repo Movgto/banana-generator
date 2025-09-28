ee514db5d1a847bc802f909f1c65e5bcadf45e30
Gumaro Monroy Vazquez <maromvz@gmail.com>
2025-09-27 18:17:45 -0600

    Implement image generation feature and update environment configuration

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import streamlit as st
+from services.image_generator import generate_image
 
+st.title("Image Generator")
 
+prompt = st.text_area("Enter your image prompt:")
+
+if st.button("Generate Image"):
+    if prompt:
+        
+        results = None
+        with st.spinner(f"Generating image...", show_time=True):
+                        
+            results = generate_image(prompt)
+        
+        text, image = results
+        
+        
+        st.success(text or 'Done!')      
+        if image:            
+            st.image(image.convert(), caption="Generated Image")
+        else:
+            st.warning('The Image could not be generated.')
+    else:
+        st.warning("Please enter a prompt to generate an image.")
--- services/image_generator.py
+++ services/image_generator.py
@@ -12,27 +12,6 @@ client = genai.Client(
     api_key= os.environ.get('API_KEY')
 )
 
-prompt = (
-    '''        
-        An image of a survival-horror game located on a typical mexican town.        
-        Kizin, one of the deities of the mayan underworld has sent horrible paranormal creatures
-        sobrenaturales con aspecto cadaverico, llamados los Kimen, la descripción es la siguiente:
-        with cadaveric aspect, they're called Kimen, their description is the following:
-
-        La palabra "kimen" significa "muerto" o "cadáver" en la lengua maya yucateca, y los kimen son criaturas de Xibalbá(el inframundo maya)
-        creadas para erradicar a la humanidad. A diferencia de los zombis modernos, los kimen se descomponen y la enfermedad se propaga al simple contacto,
-        contaminando todo a su paso.
-
-        The word "kimen" means "dead" or "corpse" in the yucateca maya tongue, and the kimen are creatures of Xibalba, the mayan underworld
-        created to extermine humanity. In contrast to modern zombies, the kimen are constantly in state of decomposition
-        and the sickness propagates only with their touch, contaminating everything in their path.
-        
-        A kid is visible, the player, it has just went out of his house and finds the streets infested of kimen.
-
-        The game is in a 3rd person view, and the graphics are PS1 style.      
-    '''
-)
-
 def generate_image(prompt: str):    
 
     response = client.models.generate_content(
@@ -41,14 +20,16 @@ def generate_image(prompt: str):
     )
 
     print(response)
-
+    text = None
+    image = None
     if response and response.candidates and response.candidates[0].content and response.candidates[0].content.parts:
         for part in response.candidates[0].content.parts:
+            results = set()
             if part.text is not None:
                 print(part.text)
+                text = part.text
             elif part.inline_data and part.inline_data.data:
                 print('Generando imagen...')            
-                image: Image.Image = Image.open(BytesIO(part.inline_data.data))
-                return image
-
-        return None          
+                image = Image.open(BytesIO(part.inline_data.data))
+                                        
+    return (text, image)
